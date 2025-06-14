933eafe140b300ef04980db736ef08633d9122d5
xiaomianGit <31613982+xiaomianGit@users.noreply.github.com>
2025-06-14 21:02:06 +0800

    Create plusOne.go

---
+++ plusOne.go
@@ -0,0 +1,17 @@
+func plusOne(digits []int) []int {
+    len := len(digits)
+
+	for i := len - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i] = digits[i] + 1
+            for j := i+1; j<len; j++{
+                digits[j] = 0
+            }
+			return digits
+		}
+	}
+
+	temp := make([]int, len+1)
+	temp[0] = 1
+	return temp
+}
